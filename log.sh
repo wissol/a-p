2c3e09e8b7a7764db95078cf42ff077e4545a99a
Miguel de Luis <miguel@sabiavida.com>
2013-12-24 15:14:30 +0000

    Create log.sh

---
+++ log.sh
@@ -0,0 +1,5 @@
+# simple shell script to update your log on the command line 
+# usage .log "your entry" or ./.log "your entry" if not on the appropiate folder (try usr/bin)
+# output appends full date + time and then whatever is on "your entry"
+#i!/bin/bash
+echo `date` $1 >> .log.txt
